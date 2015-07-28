4adcc210cdc8ee054316a9af49a5ce81465d58d4
katsumasa <kachu.h.7777@icloud.com>
2015-07-28 11:39:09 +0000

    祝！初コミット

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
+  
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
@@ -40,4 +41,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   # deviseの設定 
 config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  
+  BetterErrors::Middleware.allow_ip! '54.251.122.0'
+  
 end
